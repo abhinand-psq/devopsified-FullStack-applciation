33f986e27ab260aae813aa48734d3cf9eeff93df
abhinand-psq <abhinandpsq2@gmail.com>
2026-08-23 14:26:19 +0530

    make changes on providers

--- eks-setup/eks-build/backend.tf
+++ eks-setup/eks-build/backend.tf
@@ -6,11 +6,16 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 6.55.0"
     }
+    random = {
+  source  = "hashicorp/random"
+  version = "~> 3.7.0"
+}
   }
+  
   backend "s3" {
-    bucket         = "dev-aman-tf-bucket"
+    bucket         = "my-eks-bucket-503026942664-ap-south-1-an"
     region         = "us-east-1"
-    key            = "devopsified-FullStack-applciation/eks-setup/eks-build/terraform.tfstate"
+    key            = "eks-setup/eks-build/terraform.tfstate"
     encrypt        = true
     use_lockfile         = true
   }

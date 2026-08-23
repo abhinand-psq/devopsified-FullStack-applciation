baab38fb14a1487be180e3be59d041d0d0d6cf89
abhinand-psq <abhinandpsq2@gmail.com>
2026-08-23 14:56:59 +0530

    change s3 name

--- Jenkins-Server-TF/backend.tf
+++ Jenkins-Server-TF/backend.tf
@@ -3,10 +3,10 @@ terraform {
   backend "s3" {
     bucket               = "my-eks-bucket-503026942664-ap-south-1-an"
     region               = "ap-south-1"
-    key                  = "devopsified-FullStack-applciation/Jenkins-Server-TF/terraform.tfstate"
+    key                  = "Jenkins-Server-TF/terraform.tfstate"
     use_path_style       = false
     encrypt              = true
-    use_lockfile = true
+    use_lockfile         = true
   }
 }
 

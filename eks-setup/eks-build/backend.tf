7473fcf3a68d5503f6c9ae2946d606d00f607807
abhinand-psq <abhinandpsq2@gmail.com>
2026-09-04 12:11:56 +0530

    Update required Terraform version constraint in backend.tf

--- eks-setup/eks-build/backend.tf
+++ eks-setup/eks-build/backend.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = ">= 1.14.0, < 1.15.0"
+  required_version = ">= 1.14.0, < 2.0.0"
 
   required_providers {
     aws = {
@@ -24,3 +24,6 @@ terraform {
 provider "aws" {
   region = var.aws-region
 }
+
+
+
